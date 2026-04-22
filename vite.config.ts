4eea5106e45898133044ad299a47895e38b0e6ff
2452327415qh-debug <2452327415qh@gmail.com>
2026-04-22 21:33:09 +0800

    添加cesium.js投影

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,28 @@
-import { defineConfig } from "vite";
+import path from "path";
 import vue from "@vitejs/plugin-vue";
-import cesium from "vite-plugin-cesium";
 import glsl from "vite-plugin-glsl";
+import cesium from "vite-plugin-cesium";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    root: "example",
+    plugins: [vue(), glsl(), cesium()],
     base: "./",
-    plugins: [
-        vue(),
-        cesium(),
-        glsl()
-    ],
+    root: "example",
+    resolve: {
+        alias: {
+            "vid3d-projection": path.resolve(__dirname, "src/index.ts"),
+        },
+    },
     build: {
-        outDir: "../docs",
+        outDir: path.resolve(__dirname, "docs"),
         emptyOutDir: true,
-    }
+        rollupOptions: {
+            input: {
+                index: path.resolve(__dirname, "example/index.html"),
+                "three-cinema": path.resolve(__dirname, "example/three-cinema.html"),
+                "three-monitor": path.resolve(__dirname, "example/three-monitor.html"),
+                "cesium-monitor": path.resolve(__dirname, "example/cesium-monitor.html"),
+            },
+        },
+    },
 });
